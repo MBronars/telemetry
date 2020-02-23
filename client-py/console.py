67227ff4241bf7a45ea4ad6f854160feea1b507f
Richard Lin <elpato25@gmail.com>
2020-02-22 19:23:20 -0800

    client side support for sockets

--- client-py/console.py
+++ client-py/console.py
@@ -3,18 +3,30 @@ import time
 
 import serial
 
-from telemetry.parser import TelemetrySerial
+from telemetry.parser import TelemetrySerial, TelemetrySocket
 
 if __name__ == "__main__":
   import argparse
   parser = argparse.ArgumentParser(description='Telemetry packet parser example.')
-  parser.add_argument('port', metavar='p', help='serial port to receive on')
+
+  parser.add_argument('--hostname', metavar='h', help='network hostname')
+  parser.add_argument('--port', metavar='p', help='network port', default=1234)
+
+  parser.add_argument('--serial', metavar='s', help='serial port to receive on')
   parser.add_argument('--baud', metavar='b', type=int, default=38400,
                       help='serial baud rate')
   args = parser.parse_args()
-  
-  telemetry = TelemetrySerial(serial.Serial(args.port, baudrate=args.baud))
-  
+
+  telemetry = None
+  if args.serial is not None:
+    assert telemetry is None, "multiple comms methods defined in arguments"
+    telemetry = TelemetrySerial(serial.Serial(args.serial, baudrate=args.baud))
+    print(f"Opened serial port on {args.serial}: {args.baud}")
+  if args.hostname is not None:
+    assert telemetry is None, "multiple comms methods defined in arguments"
+    telemetry = TelemetrySocket(args.hostname, args.port)
+    print(f"Opened network socket on {args.hostname}: {args.port}")
+
   while True:
     telemetry.process_rx()
     time.sleep(0.1)
@@ -34,4 +46,3 @@ if __name__ == "__main__":
         print(chr(next_byte), end='')
       except UnicodeEncodeError:
         pass
-          
